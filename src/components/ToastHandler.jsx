69445ffaa6861c0fe2c46b6e7a4a0fc02d444ebb
Elman Pathan <18302016003@fudan.edu.cn>
2026-05-04 22:21:23 +0600

    fixed the usesearch param problem for vercel

--- src/components/ToastHandler.jsx
+++ src/components/ToastHandler.jsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
 import { toast } from "react-toastify";
 
 export default function ToastHandler() {
-  const searchParams = useSearchParams();
-
   useEffect(() => {
-    if (searchParams.get("logout") === "true") {
+    const params = new URLSearchParams(window.location.search);
+
+    if (params.get("logout") === "true") {
       toast.success("Successfully logged out");
+
+      // clean URL so it doesn't repeat
+      window.history.replaceState({}, "", "/");
     }
-  }, [searchParams]);
+  }, []);
 
   return null;
 }
